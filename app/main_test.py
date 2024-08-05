f47fddc07aa9ee96e2761f995791bfe60390def0
Yaroslav Pimenov <slavk0@ukr.net>
2024-08-05 18:15:50 +0300

    added a comment

--- app/main_test.py
+++ app/main_test.py
@@ -2,7 +2,7 @@ from main import return_backwards_string, get_mode
 import unittest
 import os
 
-
+#comment
 class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string = "This is my test string"
